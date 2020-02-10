9b7206be6d93a5bee2a6b39428be31a0037ca5c6
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2020-02-10 13:03:27 -0500

    created a child class called Birds that inherited methods and attributes from Animal

---
+++ src/com/itavarez/Birds.java
@@ -0,0 +1,37 @@
+package com.itavarez;
+
+public class Birds extends Animal {
+    private String name;
+    private int year;
+
+    public Birds(String name, int year, int energy) {
+        super(energy);
+        this.name = name;
+        this.year = year;
+    }
+
+    @Override
+    public String move() {
+        return "fly";
+    }
+
+    @Override
+    public String breath() {
+        return "breath with lungs";
+    }
+
+    @Override
+    public String reproduce() {
+        return "eggs";
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getYear() {
+        return year;
+    }
+}
